0a5657bdf7e3473b1d09038e74dc420c6aafd6c8
noviarrizal <noviarrizalarif@gmail.com>
2022-01-18 19:28:10 +0800

    Add Tugas 2

---
+++ hadiah-belanja.js
@@ -0,0 +1,8 @@
+// Input Total Belanja
+var totalBelanja = prompt("Total Belanja?", 0);
+
+if (totalBelanja >= 10000) {
+  alert("Selamat Anda Mendapatkan Hadiah!!!");
+} else {
+  alert("Anda Belum Beruntung");
+}
